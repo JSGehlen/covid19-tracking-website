a996e8602601278440c95bfec9c8d38e97073dd4
JSGehlen <jeremygehlen570@gmail.com>
2020-07-19 22:10:19 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { Cards, Chart, Country } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends React.Component {
+    state = {
+        data: {},
+        country: ''
+    }
+    
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    }
+
     render() {
+        const { data, country } = this.state;
+
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Country handleCountryChange={this.handleCountryChange} />
+                <Cards data={data} />
+                <Chart data={data} country={country}/>
             </div>
         )
     }
